d44b09c181cd69b49ff3bc7d478c14157fd73abf
zhangjinhui <642440250xyz@gmail.com>
2021-03-15 18:25:21 +0800

    Create python.py

---
+++ python.py
@@ -0,0 +1,128 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+name = 'john smith'
+age = 30
+is_new_painent = False
+print(name)
+
+
+name = input('what is your name?  ')
+print('hello ' + name)
+
+
+def printname(firstname, lastname):
+   print(lastname +" " + firstname)
+
+print("金辉", "zhang")
+
+
+
+print(123*153)
+
+print(3*2)
+
+print(3*2)
+
+name = ['1 2 3 4 6 8 7 ']
+print('-1')
+
+name = '''hello  
+waht are you doing?
+
+thankd you! 
+            support team'''
+print(name)
+
+print(1555555255225*225252552252222222222255485854574525787789454)
+pirnt
+print(10*1000000000)
+
+name = input('what is your name? ')
+print('hello '+ name)
+
+#表示从p开始的所有词组，也就是完整句子
+course = 'python for begainners'
+print(course[0:])
+
+#表示从y开始后面的所有词组
+course = 'python for begainners'
+print(course[2:])
+
+#中括号和冒号代表和上面的字符串一模一样！
+course = 'python for begainners'
+another = course[:]
+print(another)
+
+#中括号和冒号代表和上面的字符串一模一样！
+name = 'zhangjinhui is coder'
+another_name = name[:]
+print(another_name)
+
+#格式化字符串
+first = 'john'
+last = 'smith'
+message = first + ' [' + last + '] is a coder'
+msg = f'{first} [{last}] is a coder'
+print(msg)
+
+#把小写改成大写
+python = 'zhangjinhui are good'
+print(python.upper())
+
+#把大写改成小写
+course = 'python for begainers'
+print(course.upper())
+print(course.lower())
+
+#找到字幕在句子中的位置
+course = 'python for begainners'
+print(course.find('a'))
+#替换词语，把逗号前面的改成后面的
+course = 'python for begainners'
+print(course.replace('begainner','abslutly begainners'))
+
+
+
+#return 语句就是讲结果返回到调用的地方，并把程序的控制权一起返回
+#程序运行到所遇到的第一个return即返回（退出def块）
+def test_return(x)
+    if x > 0:
+        return x
+    else:
+        return 0
+
+x = 1
+y = 2
+def add (x, y):
+    z = x + y
+    return z
+print(add(x,y))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
